9a238aecb39e177299f3a584a13c5d974628f497
Jura273 <135257287+Jura273@users.noreply.github.com>
2023-07-26 12:22:30 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿
+// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам.
+//  Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
+//   лучше обойтись исключительно массивами.
+
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+// Загрузите задание на проверку до 2 нояб., 12:00 +03:00 UTC
+
+string[] Array1;
+            Console.WriteLine("Введите массив строк через пробел");
+            string stroka = Console.ReadLine();
+            Array1 = stroka.Split(' ');
+
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+string[] array2 = new string[Array1.Length];
+int count = 0;
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+
+SecondArrayWithIF(Array1, array2);
+PrintArray(array2);
+
